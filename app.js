e6d415ae89939f9b33ddc38730c472eed8ba1210
dlshnkLK <dilshankaliyanage@yahoo.com>
2021-04-13 23:24:48 +0530

    loadsongs added

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 const musicContainer  = document.querySelector('.music-container');
+const playBtn  = document.querySelector('#play');
+const prevBtn  = document.querySelector('#prev');
+const nextBtn  = document.querySelector('#next');
+const audio  = document.querySelector('#audio');
+const progress  = document.querySelector('.progress');
+const progressContainer  = document.querySelector('.progress-container');
+const title  = document.querySelector('#title');
+const cover  = document.querySelector('#cover');
+
+
+// Song title
+const songs = ['batanala'];
+
+// keep track of songs
+let songIndex = 2;
+
+//Initially load song info dom
+loadSong(songs[songIndex]);
+
+//update song details
+function loadSong(song){
+    title.innerHTML = song
+    audio.scr = `music/${song}.mp3`
+    cover.src = `images/${song}.jpg`
+}
+
+
+
+
+
